04864c4b9ed859bfa455c8691147290739d8f84b
Vityaendless <dvitya031190@gmail.com>
2023-05-19 18:41:47 +0300

    added transition between pages

--- test_main_page.py
+++ test_main_page.py
@@ -4,12 +4,9 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    link_of_login_page = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
-
-    reg_or_login_page = LoginPage(browser, link_of_login_page)
-    reg_or_login_page.should_be_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
